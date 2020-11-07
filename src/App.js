f789e6a7b209a857fc51e261a61a73360281539f
jamesNguyen <haimenguyen@gmail.com>
2020-11-07 14:26:26 -0800

    moved

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/navBar/navBar.js';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
